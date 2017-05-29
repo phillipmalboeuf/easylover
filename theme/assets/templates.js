2d8eb3599a17b83b550dfdac12985af7a3d49e35
Phillip Malboeuf <phil@boeuf.coffee>
2017-05-29 11:35:42 -0400

    Index collection

--- theme/assets/templates.js
+++ theme/assets/templates.js
@@ -1,7 +1,7 @@
 this["templates"] = this["templates"] || {};
 
 this["templates"]["cart"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data,blockParams,depths) {
-    var stack1, alias1=depth0 != null ? depth0 : {}, alias2=container.lambda, alias3=container.escapeExpression;
+    var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.lambda, alias3=container.escapeExpression;
 
   return "	<h1 class=\"highlight small_bottom text_center\">Cart</h1>\n\n"
     + ((stack1 = helpers.each.call(alias1,((stack1 = (depth0 != null ? depth0.model : depth0)) != null ? stack1.items : stack1),{"name":"each","hash":{},"fn":container.program(2, data, 0, blockParams, depths),"inverse":container.noop,"data":data})) != null ? stack1 : "")
@@ -15,7 +15,7 @@ this["templates"]["cart"] = Handlebars.template({"1":function(container,depth0,h
     + alias3(alias2(((stack1 = (depth0 != null ? depth0.text : depth0)) != null ? stack1.checkout : stack1), depth0))
     + "</button>\n	</div>\n\n";
 },"2":function(container,depth0,helpers,partials,data,blockParams,depths) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "	<div class=\"cart__item\">\n		<div class=\"grid grid--tight_guttered grid--middle\">\n			<div class=\"col col--4of12\">\n				<a href=\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
@@ -51,7 +51,7 @@ this["templates"]["cart"] = Handlebars.template({"1":function(container,depth0,h
 },"3":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return ((stack1 = helpers.unless.call(depth0 != null ? depth0 : {},(data && data.last),{"name":"unless","hash":{},"fn":container.program(4, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+  return ((stack1 = helpers.unless.call(depth0 != null ? depth0 : (container.nullContext || {}),(data && data.last),{"name":"unless","hash":{},"fn":container.program(4, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"4":function(container,depth0,helpers,partials,data) {
     return "– "
     + container.escapeExpression(container.lambda(depth0, depth0))
@@ -59,14 +59,14 @@ this["templates"]["cart"] = Handlebars.template({"1":function(container,depth0,h
 },"6":function(container,depth0,helpers,partials,data) {
     var helper, alias1=container.escapeExpression;
 
-  return alias1(((helper = (helper = helpers.key || (data && data.key)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"key","hash":{},"data":data}) : helper)))
+  return alias1(((helper = (helper = helpers.key || (data && data.key)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"key","hash":{},"data":data}) : helper)))
     + ": "
     + alias1(container.lambda(depth0, depth0))
     + "<br>";
 },"8":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},(data && data.last),{"name":"if","hash":{},"fn":container.program(9, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+  return ((stack1 = helpers["if"].call(depth0 != null ? depth0 : (container.nullContext || {}),(data && data.last),{"name":"if","hash":{},"fn":container.program(9, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"9":function(container,depth0,helpers,partials,data) {
     return "Size: "
     + container.escapeExpression(container.lambda(depth0, depth0));
@@ -84,21 +84,21 @@ this["templates"]["cart"] = Handlebars.template({"1":function(container,depth0,h
   return "\n<a href=\"#\" class=\"cart__back\" data-hide></a>\n\n\n\n<form action=\"/cart\" method=\"post\" class=\"cart__container\" id=\"cart__form\" novalidate>\n	<div class=\"alert cart__alert fades_out fade_out\" data-cart-error><div class=\"padded\"><span data-cart-error-text></span> <a href=\"#\" class=\"a--underline\" data-close-error>"
     + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.text : depth0)) != null ? stack1.alert_close : stack1), depth0))
     + "</a></div></div>\n\n\n	<a href=\"#\" class=\"cart__close a--highlight\" data-hide>X Close</a>\n\n"
-    + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},((stack1 = (depth0 != null ? depth0.model : depth0)) != null ? stack1.item_count : stack1),{"name":"if","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.program(13, data, 0, blockParams, depths),"data":data})) != null ? stack1 : "")
+    + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = (depth0 != null ? depth0.model : depth0)) != null ? stack1.item_count : stack1),{"name":"if","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.program(13, data, 0, blockParams, depths),"data":data})) != null ? stack1 : "")
     + "</form>\n\n";
 },"useData":true,"useDepths":true});
 
 this["templates"]["feed"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data,blockParams,depths) {
     var stack1;
 
-  return ((stack1 = helpers["if"].call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.link : depth0),{"name":"if","hash":{},"fn":container.program(2, data, 0, blockParams, depths),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+  return ((stack1 = helpers["if"].call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.link : depth0),{"name":"if","hash":{},"fn":container.program(2, data, 0, blockParams, depths),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"2":function(container,depth0,helpers,partials,data,blockParams,depths) {
     var stack1, helper, alias1=container.lambda, alias2=container.escapeExpression;
 
   return "<div class=\"col\" style=\"width: "
     + alias2(alias1((depths[1] != null ? depths[1].width : depths[1]), depth0))
     + "\">\n	<a href=\""
-    + alias2(((helper = (helper = helpers.link || (depth0 != null ? depth0.link : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"link","hash":{},"data":data}) : helper)))
+    + alias2(((helper = (helper = helpers.link || (depth0 != null ? depth0.link : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"link","hash":{},"data":data}) : helper)))
     + "\" target=\"_blank\" class=\"\">\n		<img src=\""
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.images : depth0)) != null ? stack1.low_resolution : stack1)) != null ? stack1.url : stack1), depth0))
     + "\" alt=\""
@@ -108,5 +108,5 @@ this["templates"]["feed"] = Handlebars.template({"1":function(container,depth0,h
     var stack1;
 
   return "\n"
-    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},(depth0 != null ? depth0.feed : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.feed : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0, blockParams, depths),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"useData":true,"useDepths":true});
